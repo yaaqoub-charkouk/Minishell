97eee1cdba43fe6df5b7a136ea71f76d2e68b046
Adnane Kh <81543774+AdnaneKharkhour@users.noreply.github.com>
2025-04-22 19:13:19 +0100

    Adnane (#12) handled redirections
    * new branches
    
    * resolved syntax for operators and quotes
    
    * need to handle syntax for < && >
    
    * handled redirections

--- minishell.h
+++ minishell.h
@@ -49,5 +49,5 @@ t_tree	*create_tree(t_list *list);
 t_list	*new_node(char *content);
 void	free_matrix(char **tokens);
 // parsing adnan
-int	is_syntax_error(char *line);
+int	is_syntax_error(char *line, t_list	*list);
 #endif
