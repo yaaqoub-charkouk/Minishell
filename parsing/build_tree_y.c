47ecb18b3a1910fd90b4bcb6c0009dd359703166
Adnane Kh <81543774+AdnaneKharkhour@users.noreply.github.com>
2025-05-04 13:00:25 +0100

    tree-1.0 (#32):
    Co-authored-by: yaaqoub <yaaqoubcourses@gmail.com>

--- parsing/build_tree_y.c
+++ parsing/build_tree_y.c
@@ -1,5 +1,23 @@
 #include "../minishell.h"
 
+// t_tree	*help(t_list	*r_list, t_list	*right, t_list	*left)
+// {
+// 	t_tree *tree_root;
+
+// 	tree_root = NULL;
+// 	if (!right || !left)
+// 		return (tree_root);
+// 	tree_root = new_tree_node(r_list);
+// 	if (right && right->type != CMD)
+// 		tree_root->right = help(right, left, left->next);
+// 	else
+// 	{
+// 		tree_root->right = new_tree_node(right);
+// 		tree_root->left = new_tree_node(left); // in the case of one operator with two command
+// 	}
+// 	return (tree_root);
+// }
+
 t_list  *new_node_queue_list(t_queue *queue) // ,t_list *list)
 {
 	t_list	*node;
@@ -14,7 +32,7 @@ t_list  *new_node_queue_list(t_queue *queue) // ,t_list *list)
 	return (node);
 }
 
-int     push(t_queue *queue, t_list **list)
+int	push(t_queue *queue, t_list **list)
 {
 	t_list	*node;
 
@@ -34,10 +52,24 @@ void	print_list(t_list	*list)
 	printf("\n");
 }
 
+void print_tree(t_tree *root) {
+	if (root == NULL) {
+		return;
+	}
+	
+	printf("%s\n", root->cmd);
+	
+	print_tree(root->left);
+	print_tree(root->right);
+}
+
+
 t_tree *new_tree_node(t_list	*token)
 {
 	t_tree	*tree_node;
 
+	if (!token)
+		return (NULL);
 	tree_node = malloc(sizeof(t_tree));
 	tree_node->args = ft_split(token->content, ' ');
 	tree_node->cmd = token->content; // a copy from the first allocated token ;
@@ -47,9 +79,35 @@ t_tree *new_tree_node(t_list	*token)
 	return (tree_node);
 }
 
+
+
+
+t_tree *build_tree_from_rqueue(t_list **current)
+{
+	t_tree *node;
+
+	if (!current || !*current)
+		return (NULL);
+
+	node = new_tree_node(*current);
+	*current = (*current)->next;
+
+	if (node->type != CMD)
+	{
+		node->right = build_tree_from_rqueue(current); // build the right first , so that the left will be the right->next ,wich will be updated when calling the function 
+		node->left = build_tree_from_rqueue(current);
+	}
+	return (node);
+}
+
+
+
+
+
 t_tree  *build_tree(t_list	*tokens)
 {
 	t_list	*reversed_queue;
+	t_list	*temp;
 	t_queue *queue;
 	t_tree *root;
 
@@ -61,10 +119,10 @@ t_tree  *build_tree(t_list	*tokens)
 		push(queue, &reversed_queue);
 		queue = queue->next;
 	}
-	// while (reversed_queue)
-	// {
-		
-	// }
-	print_list(reversed_queue);
+	temp = reversed_queue; // to free it later;
+	// root = build_tree_from_rqueue(&reversed_queue);
+	root = build_tree_from_rqueue(&reversed_queue);
+	// print_list(temp);
+	print_tree(root);
 	return (NULL);
 }
