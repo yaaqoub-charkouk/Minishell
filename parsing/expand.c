6e934c906b82015072acf219cef0b5c175f34476
Yaaqoub-charkouk <yaaqoubcourses@gmail.com>
2025-06-08 11:48:47 +0000

    fixing expand logic IFS spliting

--- parsing/expand.c
+++ parsing/expand.c
@@ -35,28 +35,111 @@ int	in_quotes(char c, int *in_dquotes, int *in_squotes, int *i)
 	return (0);
 }
 
-char	*expand_string(char **args, int	k)
+char	*get_var_value(t_env *env, char *value, int *i)
+{
+	int	len;
+	
+	len = 0;
+	
+
+	while (value[len] && ft_isalnum(value[len]))
+	{
+		len++;
+	}
+	if (i && len)
+		*i += len - 1;
+	while (env)
+	{
+		if (ft_strncmp(env->content, value, len) == 0 
+			&& env->content[len] == '=')
+			return (env->content + len + 1); // should we re alloc for the returned string ;
+		env = env->next;
+	}
+	return (ft_strdup("")); // return empty string if not found;
+}
+int	ft_argslen(char **arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+		i++;
+	return (i);
+}
+void	insert_variable(char ***args, char **pile, char *value, int *k)
+{
+	int		j;
+	int		v;
+	int		size;
+	char	**var;
+	char	**new_args;
+
+	var = ft_split_pipex(value, ' '); // free it at the end of this function ;
+	if (!var)
+		return ;
+	size = ft_argslen(*args) + ft_argslen(var);
+	new_args = malloc(size * sizeof(char *));
+	if (!new_args)
+		return ;
+	j = 0;
+	v = 0;
+	while (j < *k) // copy previous args; (*args)[j] && 
+	{
+		new_args[j] = ft_strdup((*args)[j]);
+		j++;
+	}// j == *k;
+	new_args[(*k)++] = ft_strjoin(*pile, var[v++]);
+	while (var[v]) // add the variable args ;
+		new_args[(*k)++] = ft_strdup(var[v++]);
+	v = *k;
+	(*k)--;
+	while ((*args)[j])
+		new_args[v++] = ft_strdup((*args)[j++]);
+	new_args[v] = NULL;
+	*pile = ft_strdup(new_args[*k]);
+
+	// free (var) , free(old args);
+	*args = new_args;
+}
+
+char	*expand_string(t_data *data, char ***args, int	*k)
 {
 	int		in_dquotes;
 	int		in_squotes;
 	int		i;
 	char	*pile;
+	char	*var_value;
+	char	*arg;
 
 	in_dquotes = 0;
 	in_squotes = 0;
 	i = 0;
 	pile = ft_strdup(""); // init the pile to pile on it;
-	while (args[k][i])
+	arg = ft_strdup((*args)[*k]);
+
+	while (arg[i])
 	{
-		if (in_quotes(args[k][i], &in_dquotes, &in_squotes, &i))
-			continue ; // if in quotes skip it ;
-		
-		printf("pile is  : %d %c %s\n", i, args[k][i],pile);
-		pile = accumulate_char(pile, args[k][i]); // accumulate the char to pile;
-		printf("pile is  : %d %c %s\n", i, args[k][i],pile);
+		if (in_quotes(arg[i], &in_dquotes, &in_squotes, &i))
+			continue ; // if in quotes s*kip it ;
+		/*----------------------------------------------------------------------------------------------------*/
+		if (arg[i] == '$' && ft_isalnum(arg[i + 1]) && !in_squotes) // we have variable to expand ;
+		{
+			var_value = get_var_value(*data->envl, arg + i + 1, &i); // get the var value , and skip the var name ;
+			if (in_dquotes)
+				pile = ft_strjoin(pile, var_value);
+			else
+			{
+				insert_variable(args, &pile, var_value, k);
+				// free(var_value); var_value = NULL;
+			}
+		}
+		else
+			pile = accumulate_char(pile, arg[i]); // accumulate the char to pile;
 		i++;
 	}
-	return (pile);
+	(*args)[*k] = pile; // k is the last known arg ;
+	
+	return (pile); // if you want to expand string by string ;
 }
 
 char	**ft_expand(char *cmd, t_data *data, int  *should_expand)
@@ -66,10 +149,15 @@ char	**ft_expand(char *cmd, t_data *data, int  *should_expand)
 
 	k = 0;
 	args = ft_split_pipex(cmd, ' ');
+	if (!args)
+		return (NULL);
 	while (args[k])
 	{
-		args[k] = expand_string(args, k);
-		printf("args %d : %s\n", k, args[k]);
+		printf("before exp str k is %d\n", k);
+		expand_string(data, &args, &k); // the function shold be void ; updating the args and k ;
+		printf("after exp str k is %d\n", k);
+		if(!args[k])
+			break ;
 		k++;
 	}
 	return (args);
